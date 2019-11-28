75ba5b0d089ee65a5e142562b57897e3263cb086
Elias Brucart <eliasbrucart@hardgames.com.ar>
2019-11-28 20:23:18 -0300

    Added the basic code of the game loop

--- src/Screens/game.cpp
+++ src/Screens/game.cpp
@@ -1,10 +1,49 @@
 #include "game.h"
 
-Game::Game()
-{
-}
+#include "raylib.h"
 
 
-Game::~Game()
+namespace Game
 {
+	GameState gameState = GameState::StartMenu;
+
+	const float screenWidth = 680.0f;
+	const float screenHeight = 480.0f;
+	const int minScreenWidth = 0;
+	const int minScreenHeight = 0;
+
+	void GameLoop()
+	{
+		while (!WindowShouldClose())
+		{
+			BeginDrawing();
+			ClearBackground(BLACK);
+			if (gameState == GameState::StartMenu)
+			{
+
+			}
+			if (gameState == GameState::Instructions)
+			{
+
+			}
+			if (gameState == GameState::Game)
+			{
+
+			}
+			if (gameState == GameState::FinalMenu)
+			{
+
+			}
+			if (gameState == GameState::Credits)
+			{
+
+			}
+			if (gameState == GameState::CloseAll)
+			{
+
+			}
+			EndDrawing();
+		}
+	}
+
 }
--- src/Screens/game.h
+++ src/Screens/game.h
@@ -1,9 +1,30 @@
 #ifndef GAME_H
 #define GAME_H
-class Game
+
+#include "raylib.h"
+
+namespace Game
 {
-public:
-	Game();
-	~Game();
-};
+	enum GameState
+	{
+		StartMenu,
+		Instructions,
+		Game,
+		FinalMenu,
+		Credits,
+		CloseAll
+	};
+
+	extern GameState gameState;
+
+	extern const float screenWidth;
+	extern const float screenHeight;
+	extern const int minScreenWidth;
+	extern const int minScreenHeight;
+
+	extern Music backgroundMusic;
+
+	void GameLoop();
+}
+
 #endif
