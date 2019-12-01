7c07a273e8d238e8332ce645df4bc9f0b95440f1
Elias Brucart <eliasbrucart@hardgames.com.ar>
2019-12-01 04:57:41 -0300

    Added the position of the tubes

--- src/GameObjects/pipes.cpp
+++ src/GameObjects/pipes.cpp
@@ -9,6 +9,8 @@ namespace Game
 	const int sizePipes = 4;
 	Pipes tube[sizePipes] = { 0 };
 
+	static int playerWidth = 80;
+
 	void InitPipes()
 	{
 		for (int i = 0; i < sizePipes; i++)
@@ -48,19 +50,23 @@ namespace Game
 	{
 		for (int i = 0; i < sizePipes; i++)
 		{
-			if (tube[0].rec.x <= minScreenWidth && tube[1].rec.x <= minScreenWidth)
+			if (tube[0].rec.x <= minScreenWidth - playerWidth && tube[1].rec.x <= minScreenWidth - playerWidth)
 			{
 				tube[0].rec.x = 800.0f;
-				tube[0].rec.height = GetRandomValue(0, 250);
+				tube[0].rec.y = 0.0f;
+				tube[0].rec.height = GetRandomValue(200, 250);
 				tube[1].rec.x = 800.0f;
-				tube[1].rec.height = GetRandomValue(50, 250);
+				tube[1].rec.y = 400.0f;
+				tube[1].rec.height = GetRandomValue(50, 100);
 			}
-			if (tube[2].rec.x <= minScreenWidth && tube[3].rec.x <= minScreenWidth)
+			if (tube[2].rec.x <= minScreenWidth - playerWidth && tube[3].rec.x <= minScreenWidth - playerWidth)
 			{
 				tube[2].rec.x = 1100.0f;
-				tube[2].rec.height = GetRandomValue(50, 250);
+				tube[2].rec.y = 0.0f;
+				tube[2].rec.height = GetRandomValue(200, 250);
 				tube[3].rec.x = 1100.0f;
-				tube[3].rec.height = GetRandomValue(0, 250);
+				tube[3].rec.y = 400.0f;
+				tube[3].rec.height = GetRandomValue(50, 100);
 			}
 		}
 	}
