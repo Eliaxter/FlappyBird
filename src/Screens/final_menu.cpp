55195b810093756164b9083a37c643676ea8135e
Elias Brucart <eliasbrucart@hardgames.com.ar>
2019-12-02 18:44:38 -0300

    Changed final menu

--- src/Screens/final_menu.cpp
+++ src/Screens/final_menu.cpp
@@ -4,22 +4,28 @@
 
 #include "game.h"
 #include "gameplay.h"
+#include "gameobjects/player.h"
 
 namespace Game 
 {
 	static int fontSize = 20;
+	static int fontSize2 = 30;
+	static int fontSize3 = 50;
 
-	static int coordTxtX = 300;
-	static int coordTxtY = 160;
+	static int coordTxtX = 200;
+	static int coordTxtY = 100;
 
-	static int coordTxt2X = 300;
-	static int coordTxt2Y = 200;
+	static int coordTxt2X = 240;
+	static int coordTxt2Y = 160;
 
-	static int coordTxt3X = 300;
+	static int coordTxt3X = 260;
 	static int coordTxt3Y = 220;
 
-	static int coordTxt4X = 300;
-	static int coordTxt4Y = 240;
+	static int coordTxt4X = 260;
+	static int coordTxt4Y = 260;
+
+	static int coordTxt5X = 260;
+	static int coordTxt5Y = 300;
 
 	void InputFinalMenu() 
 	{
@@ -44,13 +50,15 @@ namespace Game
 
 	void DrawTextFinalMenu() 
 	{
-		DrawText("End of Game!", coordTxtX, coordTxtY, fontSize, RAYWHITE);
+		DrawText("End of Game!", coordTxtX, coordTxtY, fontSize3, GRAY);
 
-		DrawText("Play Again: Y/N", coordTxt2X, coordTxt2Y, fontSize, RAYWHITE);
+		DrawText(TextFormat("Points Earned: %i", player.points), coordTxt2X, coordTxt2Y, fontSize2, GREEN);
 
-		DrawText("Return to Main Menu: M", coordTxt3X, coordTxt3Y, fontSize, RAYWHITE);
+		DrawText("Play Again: Y/N", coordTxt3X, coordTxt3Y, fontSize, RAYWHITE);
 
-		DrawText("Credits: C", coordTxt4X, coordTxt4Y, fontSize, RAYWHITE);
+		DrawText("Return to Main Menu: M", coordTxt4X, coordTxt4Y, fontSize, RAYWHITE);
+
+		DrawText("Credits: C", coordTxt5X, coordTxt5Y, fontSize, RAYWHITE);
 	}
 
 	void FinalMenuScreen()
